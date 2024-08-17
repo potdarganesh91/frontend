0870c7d6e9d5b9aa9eac95f584a48fc2c5a0b8bd
Ganesh <potdarganesh91@gmail.com>
2024-08-17 14:20:06 +0530

    first commit

--- src/config.js
+++ src/config.js
@@ -1,7 +1,7 @@
 // config.js
 const config = {
-  baseUrl: 'http://localhost:5000',
-  // baseUrl: 'https://backend-wcqv.onrender.com',
+  // baseUrl: 'http://localhost:5000',
+  baseUrl: 'http://www.api.gpwebapp.online',
 }
 
 export default config
